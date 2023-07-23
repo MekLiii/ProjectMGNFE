7084dfbc0cddfb58612f973becca45d6a575882e
Kamil <theczacharek@gmail.com>
2023-07-23 14:42:07 +0200

    Improved protected route

--- src/utils/ProtectedRoute.tsx
+++ src/utils/ProtectedRoute.tsx
@@ -1,28 +1,16 @@
-import { Navigate, useNavigate } from "react-router-dom";
-import { useLayoutEffect } from "react";
-import { useState } from "react"; // Import useState hook to handle initial state loading
-
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 interface ProtectedRouteProps {
-  user: {
-    email: string;
-    nameid: number;
-    Guid: string;
-    token: string;
-  };
   children: React.ReactNode;
 }
 
-const ProtectedRoute = ({ user, children }: ProtectedRouteProps) => {
-  const navigate = useNavigate();
-  const [isLoading, setIsLoading] = useState(true); // State to handle initial loading
-
-  // if (isLoading) {
-  //   return <div>Loading</div>;
-  // }
-  if (!user.token) {
+const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+  const state = useSelector((state: any) => state.auth);
+  console.log(state.token);
+  if (state.token === null) {
     return <Navigate to="/" />;
   }
- 
+
   return <>{children}</>;
 };
 
