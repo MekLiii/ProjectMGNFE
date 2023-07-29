ab1cb05bbbdcae123484cea86e4cfde48d37c167
Kamil <theczacharek@gmail.com>
2023-07-30 00:09:29 +0200

    Fixed bug with auto loginIn

--- src/pages/LoginPage/utils/LoginFromStorage.ts
+++ src/pages/LoginPage/utils/LoginFromStorage.ts
@@ -3,21 +3,22 @@ import { IDecodeToken } from "../types";
 
 export const LoginFromStorage = () => {
   const token = localStorage.getItem("token");
-  const decodeToken = token !== "null"
-    ? (jwtDecode(token as string) as IDecodeToken)
-    : null;
-  const expireDate = decodeToken ? new Date(decodeToken?.exp * 1000) : null;
-  if (expireDate && expireDate < new Date()) {
-    localStorage.removeItem("token");
-    return null;
-  }
-  if (decodeToken && token) {
-    return {
-      token,
-      nameid: parseInt(decodeToken.nameid),
-      email: decodeToken.email,
-      Guid: decodeToken.Guid,
-    };
+  if (token) {
+    const decodeToken =
+      token !== "null" ? (jwtDecode(token as string) as IDecodeToken) : null;
+    const expireDate = decodeToken ? new Date(decodeToken?.exp * 1000) : null;
+    if (expireDate && new Date(expireDate) < new Date()) {
+      localStorage.removeItem("token");
+      return null;
+    }
+    if (decodeToken && token) {
+      return {
+        token,
+        nameid: parseInt(decodeToken.nameid),
+        email: decodeToken.email,
+        Guid: decodeToken.Guid,
+      };
+    }
   }
   return null;
 };
